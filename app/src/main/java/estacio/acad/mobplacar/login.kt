e56955c2a0fc30fa6fffc9343a217ff9c6731a91
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-04-23 15:19:59 -0300

    Criando tela de login

---
+++ app/src/main/java/estacio/acad/mobplacar/login.kt
@@ -0,0 +1,11 @@
+package estacio.acad.mobplacar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
