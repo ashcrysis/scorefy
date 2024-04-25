4d0a05cd1f4036cd1176a16e0c638454f6e42476
j0jds <mrlucassilva7342@gmail.com>
2024-04-24 21:59:54 -0300

    Criação da classe placar com o referenciamento da mesma em xml e alguns testes temporários com botões.

--- app/src/main/java/estacio/acad/mobplacar/placar.kt
+++ app/src/main/java/estacio/acad/mobplacar/placar.kt
@@ -3,27 +3,34 @@ package estacio.acad.mobplacar
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class menu : AppCompatActivity() {
+class placar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_placar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val button: Button = findViewById(R.id.new_game_button)
 
-        button.setOnClickListener {
-            val intent = Intent(this, placar::class.java)
-            intent.putExtra("teste" , "Você clicou no botão.." )
-            startActivity(intent);
         }
+        val button: Button = findViewById(R.id.botaoaumentarnumero)
+        val intent: Intent = getIntent()
+        val texto: String = intent.getStringExtra("teste").toString()
+        button.setOnClickListener {
+
+            val toast =
+                Toast.makeText(this, texto, Toast.LENGTH_LONG) // in Activity
+            toast.show()
+
+        }
+
+
     }
 }
