f9279e7ce373d67c876c07311a8e036ccccb8d9d
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-04-29 15:07:04 -0300

    Login activity now hides/unhide password when clicking the button

--- app/src/main/java/estacio/acad/mobplacar/login.kt
+++ app/src/main/java/estacio/acad/mobplacar/login.kt
@@ -3,15 +3,20 @@ package estacio.acad.mobplacar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.isVisible
 
 class login : AppCompatActivity() {
+    var clicked:Boolean = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_login)
         val auth = simpleAuth();
         auth.addUser("asher","123456")
@@ -19,6 +24,7 @@ class login : AppCompatActivity() {
         val button: Button = findViewById(R.id.execute_login_button)
         val edit_name: EditText = findViewById(R.id.ed_username)
         val edit_senha: EditText = findViewById(R.id.ed_senha)
+        val buttonViewPassword: Button = findViewById(R.id.hide_password)
         button.setOnClickListener {
             val texto_nome: String = edit_name.text.toString()
             val texto_senha: String = edit_senha.text.toString()
@@ -35,5 +41,13 @@ class login : AppCompatActivity() {
             }
 
         }
+        buttonViewPassword.setOnClickListener {
+            edit_senha.tag = !((edit_senha.tag ?: false) as Boolean)
+            edit_senha.inputType = if (edit_senha.tag as Boolean)
+                InputType.TYPE_TEXT_VARIATION_PASSWORD
+            else
+                (InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
+ }
+        }
     }
-}
+
