59d29d2183cfadd1d06a185293989a056050889a
j0jds <mrlucassilva7342@gmail.com>
2024-05-15 20:27:14 -0300

    Update cadastro.kt, now the intent of register takes to the create account screen

--- app/src/main/java/estacio/acad/mobplacar/cadastro.kt
+++ app/src/main/java/estacio/acad/mobplacar/cadastro.kt
@@ -9,12 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class cadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
